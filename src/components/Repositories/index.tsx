d68694fd10319e24faefccf4c23b01cc008db855
pedromaranini <pedrolucasmaranini30@gmail.com>
2021-07-30 18:21:22 -0300

    :sparkles: add list repositories

---
+++ src/components/Repositories/index.tsx
@@ -0,0 +1,43 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+import './styles.scss';
+
+export function Repositories() {
+	const [repos, setRepos] = useState([{
+		id: '',
+		name: '',
+		language: '',
+		description: ''
+	}]);
+	
+	useEffect(() => {
+		// // REPOSITORIES
+		fetch('https://api.github.com/users/pedromaranini/repos')
+			.then((response) => {
+				return response.json();
+			})
+			.then((responseConvert) => {
+				console.log(responseConvert);
+				setRepos(responseConvert);
+			})
+	}, [])
+
+	return (
+		<main className="container">
+			<h1>REPOSITORIES</h1>
+			<div />
+			{repos.map((itemAtual) => {
+				return (
+					<section 
+						className="content"
+						key={itemAtual.id}
+					>
+						<p>{itemAtual.name}</p>
+						<span>{itemAtual.language}</span>
+						<p>{itemAtual.description}</p>						
+					</section>
+				);
+			})}
+		</main>
+	);
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,17 +1,19 @@
 import { ProfileSide } from '../../components/ProfileSide/index';
 import { AboutMe } from '../../components/AboutMe/index';
+import { Repositories } from '../../components/Repositories/index';
 
 import './styles.scss';
 
 export function Home() {
 	return (
-		<main>
+		<main className="general">
 			<div className="profileArea" style={{ gridArea: "profileArea"}}>
 				<ProfileSide />
 			</div>
 
 			<div className="aboutArea" style={{ gridArea: "aboutArea"}}>
 				<AboutMe />
+				<Repositories />
 			</div>
 		</main>
 	);
